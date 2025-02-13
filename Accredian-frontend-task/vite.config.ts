847fd8ab80320fa6ea312feaa6377ce4fd5ebc11
21prnv <pranavpatil6251@gmail.com>
2025-02-13 20:37:02 +0530

    changed database url

--- Accredian-frontend-task/vite.config.ts
+++ Accredian-frontend-task/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://accredian-task-backend-qg70.onrender.com",
     },
   },
 });
